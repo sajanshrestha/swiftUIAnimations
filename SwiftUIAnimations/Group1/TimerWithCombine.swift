bc82e58bf8e824f57231a6dd8a4f7495f28bdab7
Sajan Shrestha <sajanshrestharm11@gmail.com>
2021-01-14 14:15:05 -0500

    third commit

--- SwiftUIAnimations/Group1/TimerWithCombine.swift
+++ SwiftUIAnimations/Group1/TimerWithCombine.swift
@@ -10,24 +10,36 @@ import Combine
 
 struct TimerWithCombine: View {
     
-    @ObservedObject var counter = Counter(from: 3, to: 1)
-    @State private var newYear = false
+    @ObservedObject var counter = Counter(from: 10, to: 1)
+    
+    @State private var timeUp = false
     
     var body: some View {
         ZStack {
-            Text("\(counter.currentCount)")
-                .font(Font.custom("menlo", size: 150))
-                .foregroundColor(colors[counter.currentCount % 5])
-                .opacity(newYear ? 0 : 1)
-                .animation(.spring())
-                .onAppear { counter.count() { newYear = true } }
-            
-            HNYView()
-                .opacity(newYear ? 1 : 0)
-                .animation(.easeInOut)
-            
-            if newYear { LottieView(filename: "fireworks3") }
-            
+            if timeUp {
+                Text("Time Up")
+            }
+            else {
+                Text("\(counter.currentCount)")
+                    .font(Font.custom("menlo", size: 100))
+                    .foregroundColor(colorForIndex(index: counter.currentCount))
+                    .onAppear { counter.count {
+                        timeUp = true
+                    }}
+                ProgressCircle(startPosition: .top, lineWidth: 5, fadedColor: .green, color: .green, animationDuration: Double(counter.currentCount))
+                    .frame(width: 300, height: 300)
+            }
+        }
+    }
+    
+    private func colorForIndex(index: Int) -> Color {
+        switch index {
+        case 0...3:
+            return Color.red
+        case 4...5:
+            return Color.orange
+        default:
+            return Color.green
         }
     }
     
@@ -36,72 +48,4 @@ struct TimerWithCombine: View {
 
 }
 
-class Counter: ObservableObject {
-    
-    @Published var currentCount = 0
-    var destinationCount: Int
-    var subscriber: AnyCancellable?
-    
-    init(from initialCount: Int, to destinationCount: Int) {
-        self.currentCount = initialCount
-        self.destinationCount = destinationCount
-    }
-    
-    func count(completion: @escaping () -> Void) {
-        
-        let publisher = Timer.publish(every: 1.0, on: .main, in: .common).autoconnect()
-        
-        subscriber = publisher.sink { _ in
-            self.adjustCurrentCount(to: self.destinationCount, completion: completion)
-        }
-    }
-    
-    private func adjustCurrentCount(to endCount: Int, completion: @escaping () -> Void) {
-        if currentCount > endCount {
-            currentCount -= 1
-        }
-        else if currentCount < endCount {
-            currentCount += 1
-        }
-        else {
-            subscriber?.cancel()
-            completion()
-        }
-    }
-}
 
-
-struct HNYView: View {
-    var body: some View {
-        HStack(spacing: 40) {
-            StringyLetter(height: 300, letter: "H")
-            StringyLetter(height: 320, letter: "N")
-            StringyLetter(height: 290, letter: "Y")
-        }
-    }
-}
-
-
-struct StringyLetter: View {
-    
-    var height: CGFloat
-    var letter: String
-    
-    var body: some View {
-        VStack {
-            Rectangle()
-                .frame(width: 1, height: height)
-            Text(letter)
-                .font(.largeTitle)
-                .offset(y: -24)
-                .foregroundColor(Color.purple)
-            Spacer()
-        }
-    }
-}
-
-struct StringyLetter_Previews: PreviewProvider {
-    static var previews: some View {
-        StringyLetter(height: 1.0, letter: "")
-    }
-}
--- SwiftUIAnimations/SwiftUIAnimationsApp.swift
+++ SwiftUIAnimations/SwiftUIAnimationsApp.swift
@@ -9,9 +9,10 @@ import SwiftUI
 
 @main
 struct SwiftUIAnimationsApp: App {
+    
     var body: some Scene {
         WindowGroup {
-            PathViews()
+            PopularityView()
         }
     }
 }
